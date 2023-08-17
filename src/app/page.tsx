c4e1cb2f1d219c63fb818303037a0f1d966e9769
Henrique Penha <henrique-dsp34@hotmail.com>
2023-08-17 15:31:41 -0300

    feat(Imagineshop): Hero and integration with the back-end

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,30 @@
-import Image from 'next/image'
-import Header from './components/Header'
-import Footer from './components/Footer'
+import Banner from './components/Banner'
+import BannerImage from '../../public/banner01.png';
+import Products from './components/Products';
+import { Product } from './interfaces/product';
 
-export default function Home() {
+async function getProducts(): Promise<any> {
+  const res = await fetch('http:/localhost:3333/product', { cache: 'no-store' });
+  if(!res.ok) {
+    throw new Error('Failed to fetch data')  
+  }
+
+  const productsData: any[] = await res.json();
+  productsData.forEach(product => {
+    product.image = `http://localhost:3333/uploads/${product.fileName}`;
+    product.formattedPrice = new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(product.price);
+    product.splitPrice = new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(product.price/10);
+  })
+  return productsData;
+}
+
+export default async function Home() {
+  const products: Product[] = await getProducts()
+
   return (
     <main>
-      <h1>Hello World</h1>
+      <Banner image={BannerImage} width={1140} height={325} />
+      <Products products={products} />
     </main>
   )
 }
